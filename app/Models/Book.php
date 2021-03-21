e381f6ea4d9a26679928ebe356498e0c2e7eee2b
Jose Sustaita <josesustaita70@gmail.com>
2021-03-20 21:24:11 -0600

    added fillable fields to the author and book models

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'author_id',
+        'abstract',
+    ];
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
 }
